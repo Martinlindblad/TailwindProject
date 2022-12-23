1a085df42c6a28293adb1c0965ae23d0c00386d0
martin lindblad <martin.lindblad1@gmail.com>
2022-12-23 17:23:08 +0100

    a

---
+++ src/pages/case.tsx
@@ -0,0 +1,12 @@
+export default function Case() {
+  return (
+    <div>
+      <p>Case</p>
+      <p>This is a case</p>
+      <div className="hero">
+        <p>This is a hero</p>
+        <p>Welcome</p>
+      </div>
+    </div>
+  );
+}
